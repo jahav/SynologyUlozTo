692e900e51d2154ba545ad9550b22c053edca25d
jahav <havlicek.honza@gmail.com>
2018-02-13 00:43:06 +0100

    Rewrite more methods for analysis of the sound captcha from transcribe.js.

--- host/wave.php
+++ host/wave.php
@@ -20,11 +20,57 @@ class WaveTest extends TestCase
         $expected = array(0.2, 0.4, 0.6, 0.2, -0.2, -0.2);
         $this->assertEquals($expected, $upsampled->GetSamples());
     }
- }
+
+    // TODO: I am not really sure about this high pass filter, I would go with opposite sign
+    public function testHighPass() {
+        $wave = new Wave(array(-1, 1, 0.4, -0.7));
+        $highPass = $wave->HighPass();
+        $expected = array(-1, 0.3, 0.55, 0);
+        $this->assertEquals($expected, $highPass->GetSamples());
+    }
+
+    public function testRectify() {
+        $wave = new Wave(array(-1, 0.4, 1, -0.1, 0));
+        $highPass = $wave->Rectify();
+        $expected = array(1, 0.4, 1, 0.1, 0);
+        $this->assertEquals($expected, $highPass->GetSamples());
+    }
+
+    public function testEnvelopeResponsivness() {
+        $src = array(-4, 12, -16);
+        $wave = new Wave($src);
+        $resp = 0.25;
+        $envelope = $wave->Envelope($resp, 0.0);
+        $env0 = 0.0 * (1-$resp) + abs($src[0])*$resp;
+        $env1 = $env0*(1-$resp) + abs($src[1])*$resp;
+        $env2 = $env1*(1-$resp) + abs($src[2])*$resp;
+        $expected = array($env0, $env1, $env2);
+        $this->assertEquals($expected, $envelope->GetSamples());
+    }
+
+    public function testEnvelopeDampening() {
+        $src = array(256,255,63,15, 5);
+        $wave = new Wave($src);
+        $resp = 1;
+        $damp = 0.25;
+        $envelope = $wave->Envelope($resp, $damp);
+        $expected = array(256, 64, 16, 4, 5);
+        $this->assertEquals($expected, $envelope->GetSamples());
+    }
+
+    public function testTrigger() {
+        $wave = new Wave(array(0.5, 0.4, 0.3, 0.2, -0.3, -0.2, 0, 0.7));
+        $envelope = $wave->Trigger(-0.1, 0.4);
+        $expected = array(1, 1, 1, 1, 0, 0, 0, 1);
+        $this->assertEquals($expected, $envelope->GetSamples());
+    }
+}
 
 class Wave {
     private const WAV_HEADER_SIZE = 48;
-
+    /** How fast will envelope respond to increase in the sample value */
+    private const DEFAULT_ENVELOPE_RESPONSIVENESS = 0.2;
+    private const DEFAULT_ENVELOPE_DAMPENING = 0.995;
     private $Samples;
 
     public function __construct($Samples = null) {
@@ -80,9 +126,7 @@ class Wave {
             $downsamples[$tgt_i] = $total / $samples;
         }
         
-        $ret =  new Wave();
-        $ret->Samples = $downsamples;
-        return $ret;
+        return new Wave($downsamples);
     }
 
     public function Upsample(int $samples) {
@@ -100,13 +144,67 @@ class Wave {
             }
         }
 
-        $ret =  new Wave();
-        $ret->Samples = $upsampled;
-        return $ret;
+        return new Wave($upsampled);
     }
 
     public function GetSamples() {
         return $this->Samples;
+    }
+
+    public function GetIntervals() {
+        $waveLowPass = LowPass();
+        $waveHighPass = HighPass();
+    }
+
+    private function LowPass() {
+        return $this->Downsample(8)->Upsample(8);
+    }
+
+    public function HighPass() {
+        $highPass = array();
+        for ($i = 0; $i < count($this->Samples) - 1; $i++) {
+            $highPass[$i] = ($this->Samples[$i] - $this->Samples[$i+1])/2;
+        }
+        $highPass[count($this->Samples) - 1] = 0;
+        return new Wave($highPass);
+    }
+
+    public function Rectify() {
+        return new Wave(array_map(function ($sample) { return abs($sample); }, $this->Samples));
+    }
+
+    public function Envelope(float $responsivness = ENVELOPE_RESPONSIVENESS, float $dampening = DEFAULT_ENVELOPE_DAMPENING) {
+        $envelope = array();
+        $envelopeValue = 0;
+        for ($i = 0; $i < count($this->Samples); $i++) {
+            $sampleValue = abs($this->Samples[$i]);
+            if ($sampleValue > $envelopeValue) {
+                $envelopeValue = (1-$responsivness) * $envelopeValue + $responsivness * $sampleValue;
+            } else {
+                $envelopeValue *= $dampening;
+            }
+            $envelope[$i] = $envelopeValue;
+        }
+        return new Wave($envelope);
+    }
+
+    public function Trigger(float $low, float $high)
+    {
+        $triggers = array();
+        $trigger = 0.0;
+        
+        for ($i = 0; $i < count($this->Samples); $i++) {
+            $sampleValue = $this->Samples[$i];
+            if ($sampleValue > $high) {
+                $trigger = 1.0;
+            }
+            if ($sampleValue < $low) {
+                $trigger = 0.0;
+            }
+            $triggers[$i] = $trigger;
+        }
+
+        return new Wave($triggers);
     }
 }
 
